a95f6aaf2183768b33551557225e79d4a7ea5f41
daesung-kim <saint.pablo94@gmail.com>
2015-12-17 22:55:17 +0900

    594P

---
+++ src/main/java/com/springbook/Ch07Application.java
@@ -0,0 +1,12 @@
+package com.springbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch07Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ch07Application.class, args);
+    }
+}
