b131ecc9c2f04bf6383ff51edfd99e1f94395923
saintpablo94 <saint.pablo94@gmail.com>
2015-12-23 23:33:34 +0900

    637p

--- src/main/java/springbook/user/sqlservice/OxmSqlService.java
+++ src/main/java/springbook/user/sqlservice/OxmSqlService.java
@@ -6,6 +6,8 @@ import javax.annotation.PostConstruct;
 import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
 
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.oxm.Unmarshaller;
 
 import springbook.user.dao.UserDao;
@@ -14,6 +16,7 @@ import springbook.user.sqlservice.jaxb.Sqlmap;
 
 public class OxmSqlService implements SqlService {
 
+	private final BaseSqlService baseSqlService = new BaseSqlService();
 	private final OxmSqlReader oxmSqlReader = new OxmSqlReader();
 	private SqlRegistry sqlRegistry = new HashMapSqlRegistry();
 	
@@ -25,47 +28,57 @@ public class OxmSqlService implements SqlService {
 		this.oxmSqlReader.setUnmarshaller(unmarshaller);
 	}
 	
-	public void setSqlmapFile(String sqlmapFile){
-		this.oxmSqlReader.setSqlmapFile(sqlmapFile);
+	public void setSqlmap(Resource sqlmap){
+		this.oxmSqlReader.setSqlmap(sqlmap);
 	}
+	
+/*	public void setSqlmapFile(String sqlmapFile){
+		this.oxmSqlReader.setSqlmapFile(sqlmapFile);
+	}*/
 	
 	@PostConstruct
 	public void loadSql(){
-		this.oxmSqlReader.read(this.sqlRegistry);
+		this.baseSqlService.setSqlReader(this.oxmSqlReader);
+		this.baseSqlService.setSqlRegistry(this.sqlRegistry);
+
+		this.baseSqlService.loadSql();
 	}
 	
 	@Override
 	public String getSql(String key) throws SqlRetrievalFailureException {
-		try {
-			return this.sqlRegistry.findSql(key);
-		} catch (SqlNotFoundException e) {
-			throw new SqlRetrievalFailureException(e);
-		}
+		return this.baseSqlService.getSql(key);
 	}
 
 	private class OxmSqlReader implements SqlReader {
-		private Unmarshaller unmarshaller;
-		private final static String DEFAULT_SQLMAP_FILE = "sqlmap.xml";
-		private String sqlmapFile = DEFAULT_SQLMAP_FILE ;
 		
+		private Resource sqlmap = new ClassPathResource("sqlmap.xml", UserDao.class);
+		
+		private Unmarshaller unmarshaller;
+		/*private final static String DEFAULT_SQLMAP_FILE = "sqlmap.xml";
+		private String sqlmapFile = DEFAULT_SQLMAP_FILE ;
+		*/
+		public void setSqlmap(Resource sqlmap) {
+			this.sqlmap = sqlmap;
+		}
 		public void setUnmarshaller(Unmarshaller unmarshaller) {
 			this.unmarshaller = unmarshaller;
 		}
-		public void setSqlmapFile(String sqlmapFile) {
+		
+		/*public void setSqlmapFile(String sqlmapFile) {
 			this.sqlmapFile = sqlmapFile;
-		}
+		}*/
 		
 		public void read(SqlRegistry sqlRegistry){
 			try {
-				Source source = new StreamSource(
-						UserDao.class.getResourceAsStream(this.sqlmapFile));
+				//Source source = new StreamSource(UserDao.class.getResourceAsStream(this.sqlmapFile));
+				Source source = new StreamSource(sqlmap.getInputStream());
 				Sqlmap sqlmap = (Sqlmap) this.unmarshaller.unmarshal(source);
 				for (SqlType sql : sqlmap.getSql()) {
 					sqlRegistry.registerSql(sql.getKey(), sql.getValue());
 				}
 			} catch (IOException e) {
 				throw new IllegalArgumentException(
-						this.sqlmapFile+" 을 가져올수 없습니다."+e);
+						this.sqlmap.getFilename()+" 을 가져올수 없습니다."+e);
 			}
 		}
 	}
