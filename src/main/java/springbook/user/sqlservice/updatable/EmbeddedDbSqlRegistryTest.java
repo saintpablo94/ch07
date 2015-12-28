172c38e7f7dc27ad3cdbdc8c54a7d0a7a4ee2c64
saintpablo94 <saint.pablo94@gmail.com>
2015-12-28 22:22:32 +0900

    EmbeddedDbSqlRegistryTest 639P

--- src/main/java/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistryTest.java
+++ src/main/java/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistryTest.java
@@ -2,13 +2,12 @@ package springbook.user.sqlservice.updatable;
 
 import springbook.user.sqlservice.UpdatableSqlRegistry;
 
-public class ConcurrentHashMapSqlRegistryTest 
-	extends AbstractUpdatableSqlRegistryTest {
+public class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest {
 
 	@Override
 	protected UpdatableSqlRegistry createUpdatableSqlRegistry() {
-		return new ConcurrentHashMapSqlRegistry();
+		// TODO Auto-generated method stub
+		return null;
 	}
-	
-	
+
 }
