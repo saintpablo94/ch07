9a4ac6ed9239597bd3ed60e5fd03764f06160e94
saintpablo94 <saint.pablo94@gmail.com>
2015-12-28 22:23:34 +0900

    639p

--- src/main/java/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistryTest.java
+++ src/main/java/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistryTest.java
@@ -1,12 +1,21 @@
 package springbook.user.sqlservice.updatable;
 
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+
 import springbook.user.sqlservice.UpdatableSqlRegistry;
 
 public class EmbeddedDbSqlRegistryTest extends AbstractUpdatableSqlRegistryTest {
-
+	
+	EmbeddedDatabase db;
+	
 	@Override
 	protected UpdatableSqlRegistry createUpdatableSqlRegistry() {
-		// TODO Auto-generated method stub
+		db = new EmbeddedDatabaseBuilder()
+			.setType(HSQL)
+			.addScript().build();
+		
+			
 		return null;
 	}
 
