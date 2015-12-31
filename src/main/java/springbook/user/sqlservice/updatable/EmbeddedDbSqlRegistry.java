7e12069aca41fe505c91b821443911baf91f5f18
saintpablo94 <saint.pablo94@gmail.com>
2015-12-31 16:56:49 +0900

    650p

--- src/main/java/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistry.java
+++ src/main/java/springbook/user/sqlservice/updatable/EmbeddedDbSqlRegistry.java
@@ -6,6 +6,10 @@ import javax.sql.DataSource;
 
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallbackWithoutResult;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import springbook.user.sqlservice.SqlNotFoundException;
 import springbook.user.sqlservice.SqlUpdateFailureException;
@@ -13,9 +17,12 @@ import springbook.user.sqlservice.UpdatableSqlRegistry;
 
 public class EmbeddedDbSqlRegistry implements UpdatableSqlRegistry {
 	SimpleJdbcTemplate jdbc;
+	TransactionTemplate transactionTemplate;
 	
 	public void setDataSource(DataSource dataSource){
 		jdbc = new SimpleJdbcTemplate(dataSource);
+		transactionTemplate = new TransactionTemplate(
+				new DataSourceTransactionManager(dataSource));
 	}
 
 	@Override
@@ -50,9 +57,14 @@ public class EmbeddedDbSqlRegistry implements UpdatableSqlRegistry {
 	@Override
 	public void updateSql(Map<String, String> sqlmap)
 			throws SqlUpdateFailureException {
-		for(Map.Entry<String, String> entry : sqlmap.entrySet()){
-			updateSql(entry.getKey(),entry.getValue());
-		}
+		
+		transactionTemplate.execute(new TransactionCallbackWithoutResult() {
+			@Override
+			protected void doInTransactionWithoutResult(TransactionStatus status) {
+				for(Map.Entry<String, String> entry : sqlmap.entrySet()){
+					updateSql(entry.getKey(),entry.getValue());
+				}
+			}
+		});
 	}
-
 }
